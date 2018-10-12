b3e73bfeadd285c78a091960015a04950d2a3e39
Julian Zapata <julian.zapata@MacBook-Pro-de-Apple.local>
2018-10-12 05:58:04 -0500

    Version maraton

--- src/main/java/com/tns/maraton/controllers/LoginController.java
+++ src/main/java/com/tns/maraton/controllers/LoginController.java
@@ -1,10 +1,9 @@
 package com.tns.maraton.controllers;
 
+import com.tns.maraton.exceptions.BusinessException;
 import com.tns.maraton.model.response.RecognizeResponse;
 import com.tns.maraton.model.response.PingResponse;
-import com.tns.maraton.services.LoginServices;
-import com.tns.maraton.util.FileUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.tns.maraton.util.Constants;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,8 +13,6 @@ import java.io.IOException;
 @RestController
 public class LoginController {
 
-    @Autowired
-    private LoginServices loginServices;
 
     @GetMapping("/ping")
     public PingResponse ping(){
@@ -24,13 +21,13 @@ public class LoginController {
 
     @PostMapping("/register")
     public RecognizeResponse register(@RequestPart(value = "file") MultipartFile image, @RequestPart(value = "user") String user) throws IOException {
-        return loginServices.register(FileUtil.convert(image), user);
+        throw new BusinessException(Constants.UNDER_CONSTRUCTION);
 
     }
 
     @PostMapping("/compare")
     public RecognizeResponse compare(@RequestPart(value = "file") MultipartFile image, @RequestPart(value = "user") String user) throws IOException {
-        return loginServices.compare(FileUtil.convert(image), user);
+        throw new BusinessException(Constants.UNDER_CONSTRUCTION);
 
     }
 
